c16e0a5b2b68024c50a6d784a49fcadd530dbf0c
yomidigital <dev@yomi.digital>
2024-09-17 23:23:00 +0200

    claim

--- src/AppWindow.tsx
+++ src/AppWindow.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAccount } from "wagmi";
+import HeartImg from "./assets/images/icons/heart.svg";
 import ScrollToTop from "./components/ScrollToTop";
 
 function AppWindow({ children }: PropsWithChildren) {
@@ -17,6 +18,16 @@ function AppWindow({ children }: PropsWithChildren) {
     <>
       <ScrollToTop />
       {children}
+      <div className="pb-2 pl-2 fixed bottom-0 left-0">
+        <div className="py-1 px-2 text-black bg-white rounded-lg text-xs">
+          <span>
+            Made with <img src={HeartImg} alt="Heart" className="inline" /> by{" "}
+            <a href="https://yomi.digital" target="_blank">
+              YOMI
+            </a>
+          </span>
+        </div>
+      </div>
     </>
   );
 }
